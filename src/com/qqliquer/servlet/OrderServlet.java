ba5c123ccc3ab66e78f4fc9cc70dff30a991ecdd
wstv <wstv@DESKTOP-MPQG9B3>
2016-04-14 23:22:12 +0800

    2016年4月14日23:18:17

--- src/com/qqliquer/servlet/OrderServlet.java
+++ src/com/qqliquer/servlet/OrderServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class OrderServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
